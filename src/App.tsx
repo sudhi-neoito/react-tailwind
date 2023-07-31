70fa2592f9f8248573b7e25d57949ea1f5cca102
sudhi-neoito <sudhi@neoito.com>
2023-07-31 17:24:41 +0530

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-red-400">hello</div>
+    </div>
+  );
+}
+
+export default App;
